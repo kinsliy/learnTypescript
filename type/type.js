0429163a3bba311759a17d04930fd2cf24139fce
刘威 <liuwei001@zhongan.io>
2019-01-09 17:39:58 +0800

    type

--- type/type.js
+++ type/type.js
@@ -1,1 +1,15 @@
 var isDone = false;
+var str = 'kinsliy';
+var list = [1];
+var notSure = 21;
+var list1 = [1, "e2"];
+function warnUser() {
+    console.log("This is my waring message");
+}
+// 默认情况下null和undefined是所有类型的子类型。 就是说你可以把 null和undefined赋值给number类型的变量。
+function error(message) {
+    throw new Error(message);
+}
+create({ prop: 0 });
+var someValue = "this is a string";
+var strLength = someValue.length;
--- type/type.ts
+++ type/type.ts
@@ -1,5 +1,36 @@
+let isDone: boolean = false;
 
-let isDone:boolean =false;
+let str: string = 'kinsliy';
+
+let list: number[] = [1];
+
+let notSure: any = 21;
+
+let list1: any[] = [1, "e2"];
+
+function warnUser(): void {
+  console.log("This is my waring message");
+}
+
+// 默认情况下null和undefined是所有类型的子类型。 就是说你可以把 null和undefined赋值给number类型的变量。
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// never类型是任何类型的子类型，也可以赋值给任何类型；
+
+declare function create(o: object | null): void;
+create({ prop: 0 });
+
+
+let someValue: any = "this is a string";
+
+let strLength: number = (someValue as string).length;
+
+
+
+
 
 
 
